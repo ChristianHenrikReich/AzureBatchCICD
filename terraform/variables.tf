74b1892e18d26825c608dbbb2fb7d9687fa98d73
Christian Henrik Reich <have@Christians-MacBook-Air.local>
2025-01-06 22:16:50 +0100

    Paramterize

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,19 @@ variable "environment" {
   description = "dev, tst or prd, to descripe development, test or production. More can be added, recommendation is to keep 3 letters"
   type        = string
 }
+
+
+variable "vm_sku" {
+  description = "Azure VM Sku"
+  type        = string
+}
+
+variable "nodes" {
+  description = "Number of dedicated nodes"
+  type        = number
+}
+
+variable "spot_nodes" {
+  description = "Number of spot nodes"
+  type        = number
+}
